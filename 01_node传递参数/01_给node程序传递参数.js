db6579377db477f63adbea370571b38c9268a78f
ali <ali_ovo@qq.com>
2024-05-12 22:22:43 +0800

    init study node

---
+++ 01_node传递参数/01_给node程序传递参数.js
@@ -0,0 +1,11 @@
+console.log(process)
+
+// node index.js ali env=dev
+  // process: {
+  //   argv: [
+  //     '/usr/local/bin/node',
+  //     '/Users/ali/Documents/learn-node/01_node传递参数/01_给node程序传递参数.js',
+  //     'ali',
+  //     'env=dev',
+  //   ],
+  // }
